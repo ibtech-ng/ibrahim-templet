53446d40752f298a1a842ba4f20bd45b4e4b2e25
ibtech-ng github <oladipupoibrahim10@gmail.com>
2023-04-24 00:23:28 +0100

    project added

---
+++ static/js/javascript.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+   el.addEventListener('shown.bs.tab', () => {
+     const target = el.getAttribute('data-bs-target')
+     const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+     bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+   })
+ })
+
+
+ const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+   target: '#navbar-example'
+ })
